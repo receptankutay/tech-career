fe382cecd65a79e3ad643995e11681fa2f169627
receptankutay <tankutayrecep@gmail.com>
2022-05-13 02:06:36 +0300

    master

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Footer from "./template/Footer";
+import Header from "./template/Header";
+import Navbar from "./template/Navbar";
+import PageContent from "./template/PageContent";
+
+
+function App() {
+  return (<>
+    <Navbar></Navbar>
+    <Header></Header>
+    <PageContent></PageContent>
+    <Footer></Footer>
+  </>
+  )
+}
+
+export default App;
---
+++ src/template/PageContent.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import About from './About'
+import Contact from './Contact'
+import ImageLocation from './ImageLocation'
+import Project from './Project'
+
+function PageContent() {
+    return (
+        <>
+            <div className="w3-content w3-padding" style={{ maxWidth: 1564 }}></div>
+
+
+            <Project></Project>
+            <About></About>
+            <Contact></Contact>
+            <ImageLocation></ImageLocation>
+        </>
+    )
+}
+
+export default PageContent
